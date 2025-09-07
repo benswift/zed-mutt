e7a6654c3d4e7d583ff4f0254267ea07f0164993
Ben Swift <ben@benswift.me>
2025-09-07 14:24:29 +1000

    remove some reply-quote handling stuff which wasn't working

--- languages/mutt-compose/highlights.scm
+++ languages/mutt-compose/highlights.scm
@@ -4,15 +4,5 @@
   ":" @punctuation.delimiter
   value: (header_value) @string)
 
-; Body with different highlighting for quote levels
+; Body - styled as text, with markdown injection handling formatting
 (body) @text
-
-; Reply quote levels (these use Zed's comment scopes for visual distinction)
-((body) @comment
-  (#match? @comment "^>\\s"))
-
-((body) @comment.documentation
-  (#match? @comment.documentation "^>\\s*>\\s"))
-
-((body) @comment.block
-  (#match? @comment.block "^>\\s*>\\s*>\\s"))
